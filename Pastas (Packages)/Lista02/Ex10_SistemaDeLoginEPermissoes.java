f9611ed7f9c66f731f4da47f4a9ffef435579860
Milene Vitória <milene24dez@outlook.com>
2025-09-15 21:52:19 -0300

    Update Ex10_SistemaDeLoginEPermissoes.java

--- Pastas (Packages)/Lista02/Ex10_SistemaDeLoginEPermissoes.java
+++ Pastas (Packages)/Lista02/Ex10_SistemaDeLoginEPermissoes.java
@@ -1,7 +1,7 @@
 package Lista02;
 
 import java.util.Scanner;
-public class Ex10_SistemaDeLoginEPermissoes {
+public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -11,64 +11,74 @@ public class Ex10_SistemaDeLoginEPermissoes {
 
         String NomeDeUsuario;
         int Senha;
+        int tentativas = 0;
+        boolean acessoConcedido = false;
 
         //Login
-        System.out.print("NOME DE USUÁRIO: ");
-        NomeDeUsuario = input.next();
-        System.out.print("SENHA: ");
-        Senha = input.nextInt();
-        System.out.println(" ");
+        while (tentativas < 3 && !acessoConcedido) {
+            System.out.print("NOME DE USUÁRIO: ");
+            NomeDeUsuario = input.next();
+            System.out.print("SENHA: ");
+            Senha = input.nextInt();
+            System.out.println(" ");
 
-        //Listagem de usuários existentes
-        boolean usuarioExiste = NomeDeUsuario.equals("admin") ||
-                                NomeDeUsuario.equals("aluno") ||
-                                NomeDeUsuario.equals("professor");
+            //Listagem de usuários existentes
+            boolean usuarioExiste = NomeDeUsuario.equals("admin") ||
+                    NomeDeUsuario.equals("aluno") ||
+                    NomeDeUsuario.equals("professor");
 
-        //Administrador
-        if (NomeDeUsuario.equals("admin")) {
-            if (Senha == 1234) {
-                System.out.print("""
+            //Administrador
+            if (NomeDeUsuario.equals("admin")) {
+                if (Senha == 1234) {
+                    System.out.print("""
                         [ACESSO TOTAL]
                         
                         Bem-vindo, administrador! 👑""");}
-            else {
-                System.out.println("Senha incorreta para o usuário admin! ❌");
+                acessoConcedido = true;
+                else {
+                    System.out.println("Senha incorreta para o usuário admin! ❌");
+                }
             }
-        }
 
-        //Aluno
-        else if (NomeDeUsuario.equals("aluno")) {
-            if (Senha == 0000) {
-                System.out.print("""
+            //Aluno
+            else if (NomeDeUsuario.equals("aluno")) {
+                if (Senha == 0000) {
+                    System.out.print("""
                         [ACESSO RESTRITO]
                         
                         Bem-vindo, aluno! 🎓
                         Você pode consultar suas notas.""");
-            } else {
-                System.out.println("Senha incorreta para o usuário aluno! ❌");
+                    acessoConcedido = true;
+                } else {
+                    System.out.println("Senha incorreta para o usuário aluno! ❌");
 
+                }
             }
-        }
 
-        //Professor
-        else if (NomeDeUsuario.equals("professor")) {
-                 if (Senha == 4321) {
-            System.out.print("""
+            //Professor
+            else if (NomeDeUsuario.equals("professor")) {
+                if (Senha == 4321) {
+                    System.out.print("""
                     [ACESSO PARCIAL]
                     
                     Bem-vindo, professor! 👨‍🏫
                     Você pode lançar notas.""");}
-                 else {
-                     System.out.println("Senha incorreta para o usuário professor! ❌");
-                 }
+                    acessoConcedido = true;
+                else {
+                    System.out.println("Senha incorreta para o usuário professor! ❌");
+                }
             }
 
-        //usuário existir mas a senha estiver incorreta
+            //usuário existir mas a senha estiver incorreta
 
-        //usuário não existir
-        else {
-            System.out.println("Usuário ou senha incorretos! ❌");}
+            //usuário não existir
+            else {
+                System.out.println("Usuário ou senha incorretos! ❌");}
 
-        System.out.println(" ");
+            System.out.println(" ");
+        }
     }
 }
+
+
+
