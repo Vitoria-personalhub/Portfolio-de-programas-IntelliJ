15a03dc19e9eca79196088b5216e3cba683b9afd
Vitoria-personalhub <216695429+Vitoria-personalhub@users.noreply.github.com>
2025-09-26 17:50:23 -0300

    Java

--- Pastas (Packages)/Lista03/Ex02_FinanciamentoBancario.java
+++ Pastas (Packages)/Lista03/Ex02_FinanciamentoBancario.java
@@ -1,24 +1,45 @@
 package Lista03;
 
+// Exercício 02:
+// Faça um programa que leia:
+// - O valor da renda mensal de uma pessoa.
+// - O valor da prestação mensal que ela deseja pagar no financiamento.
+// Verifique se ela pode assumir essa prestação com segurança, segundo esta regra:
+// - O valor da prestação não pode ultrapassar 30% da sua renda mensal.
+// - Se a prestação ultrapassar R$3.000,00, exigir que a renda mensal seja pelo menos R$10.000,00.
+// Mostrar mensagens adequadas, por exemplo:
+// “Financiamento aprovado”
+// “Financiamento negado por alta prestação”
+// “Financiamento negado por baixa renda”
+
 import java.util.Scanner;
 
 public class Ex02_FinanciamentoBancario {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("FINANCIAMENTO BANCÁRIO");
+        System.out.println("\u001B[35mFINANCIAMENTO BANCÁRIO\u001B[0m");
+        System.out.print("Qual é a sua \u001B[1mRENDA MENSAL\u001B[0m? R$ ");
+        double rendamensal = input.nextDouble();
 
-        double valormensal;
-
-        System.out.print("Qual o valor da pretensão mensal que" +
+        System.out.print("Qual o valor da \u001B[1mPRESTAÇÃO MENSAL\u001B[0m que " +
                 "você pretende pagar durante o financiamento? R$ ");
-        valormensal = input.nextDouble();
+        double prestacaomensal = input.nextDouble();
 
+        System.out.println(" ");
         // Ela pode assumir a pretensão com segurança se:
+        // 1- O valor não ultrapassar 30% da sua renda mensal
+        if (prestacaomensal > 3000 && rendamensal < 10000) {
+            System.out.println("\u001B[1m\u001B[31mFinanciamento negado por baixa renda.\u001B[0m");
 
-        // O valor não ultrapassar 30% da sua renda mensal
-
-        // Se a pretensão for maior que R$  3.000, exigir
-        // que a renda mensal seja pelo menos R$ 10.000.
+            // 2 - Se a prestação > R$3.000, exigir renda mensal ≧ R$10.000
+        } else if (prestacaomensal > rendamensal * 0.3) {
+            double valorpermitido = rendamensal * 0.3;
+            System.out.printf("A prestação ultrapassa 30%% de sua renda mensal (Limite permitido: R$ %.2f)\n", valorpermitido);
+            System.out.println("\u001B[1m\u001B[31mFinanciamento negado por alta prestação.\u001B[0m");
+        } else {
+            System.out.println("\u001B[1m\u001B[32mFinanciamento aprovado.\u001B[0m");
+            System.out.println("O empréstimo poderá ser realizado com segurança.");
+        }
     }
 }
